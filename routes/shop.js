445b355dfa38a41571d3a83d0f553aa79679843c
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-03-10 13:19:52 +0100

    links, products ...
    Nothing much new.

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,9 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+//:productId je lahko karkoli in express router to ve
+//in upošteva, pomembno je, da normalne route damo nad to!!
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
